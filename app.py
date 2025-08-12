cee83d1106a0c45e953eeccd94df31c954e3ca1a
Jozhynes <jozhynespatrick@gmail.com>
2025-08-12 12:33:59 +0300

    initial

--- app.py
+++ app.py
@@ -18,12 +18,30 @@ def home():
         
     return render_template('index.html')
 
+
+
 @app.route('/show_product')
 def show_product():
     db = Sessionlocal()
     product = db.query(Product).all()  
     db.close()
     return render_template('product.html', product=product)
+
+
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        
+        print(f"Message from {name} ({email}): {message}")
+
+        return "Thank you for contacting us!"
+    
+    return render_template("contact.html")
+
 
 if __name__=='__main__':
     app.run(debug=True)
